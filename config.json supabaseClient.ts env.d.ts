8b72537377ae07e903968019805d8c26b1203425
Tyrone Fields <tfieldsvender@hotmail.com>
2025-09-16 22:16:04 -0500

    Fix TS build: correct tsconfig + env types; remove stray file

--- config.json supabaseClient.ts env.d.ts
+++
@@ -1,5 +0,0 @@
-[33mcommit 9ed418287e0d6601a04fbd217c4f5df94d59a85d[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
-Author: Tyrone Fields <tfieldsvender@hotmail.com>
-Date:   Tue Sep 16 17:37:08 2025 -0500
-
-    Auth header + infinite-sync fix; password login; UI polish
