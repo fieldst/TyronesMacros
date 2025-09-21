abec472c821598de7fb1109740e3360363d46027
unknown <tfieldsvender@hotmail.com>
2025-09-20 22:53:02 -0500

    added dev-dist

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.u31ojgpd3rg"
+    "revision": "0.u49mp4kb2e8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
